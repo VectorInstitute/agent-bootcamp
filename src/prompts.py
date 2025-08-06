2cd9c3942b5e2d58f0490483e56197748aed0bf0
Ioana Barbos <ioana.negrut@gmail.com>
2025-08-06 14:31:58 +0000

    MVP working

--- src/prompts.py
+++ src/prompts.py
@@ -1,7 +1,9 @@
 """Centralized location for all system prompts."""
 
 REACT_INSTRUCTIONS = """\
-Answer the question using the search tool. \
+Answer the question in two steps, always in the same order: \
+step 1 use the faq_match tool. If you can find the answer from the FAQs alone, say "hurray!" and give preliminary answer.\
+step 2 use the search tool to enhance the answer from the first step. \
 EACH TIME before invoking the function, you must explain your reasons for doing so. \
 Be sure to mention the sources in your response. \
 If the search tool did not return intended results, try again. \
