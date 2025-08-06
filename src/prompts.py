0193be2c982c7979fee0529c615c8212651c3533
Calen Irwin <calen.irwin@urbanandmainlines.com>
2025-08-06 12:57:35 +0000

    wip: initial multi agent orchestrator

--- src/prompts.py
+++ src/prompts.py
@@ -12,3 +12,14 @@ Do not make up information. \
 For facts that might change over time, you must use the search tool to retrieve the \
 most up-to-date information.
 """
+
+ORCHESTRATOR_REACT_INSTRUCTIONS = """\
+Answer the question using the search knowledge base agent as a tool. \
+EACH TIME before invoking the function, you must explain your reasons for doing so. \
+Be sure to mention the sources in your response. \
+If the search tool did not return intended results, try again. \
+For best performance, divide complex queries into simpler sub-queries. \
+Do not make up information. \
+For facts that might change over time, you must use the search tool to retrieve the \
+most up-to-date information.
+"""
