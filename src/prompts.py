9feedee4d7ace584ae6cb49e094ff4fe232ef4ff
Calen Irwin <calen.irwin@urbanandmainlines.com>
2025-08-06 19:33:56 +0000

    working multi-agent with prompts specifying data formats

--- src/prompts.py
+++ src/prompts.py
@@ -1,13 +1,66 @@
 """Centralized location for all system prompts."""
 
 REACT_INSTRUCTIONS = """\
-Answer the question in two steps, always in the same order: \
-step 1 use the faq_match tool. If you can find the answer from the FAQs alone, say "hurray!" and give preliminary answer.\
-step 2 use the search tool to enhance the answer from the first step. \
-EACH TIME before invoking the function, you must explain your reasons for doing so. \
-Be sure to mention the sources in your response. \
-If the search tool did not return intended results, try again. \
-For best performance, divide complex queries into simpler sub-queries. \
+You are a high level orchestration agent that has access to other agents as tools. \
+Your objective is to take a user promtp, identify relevant question/answer pairs from a QA database, \
+search for answers to the identified questions in a central knowledge base, and then evaluate the  \
+generated answer against the ground truth. You should follow this sequence: \
+
+STEP 1 - call the QA search agent with the user prompt \
+STEP 2 - call the knowledge base search agent with each of the identified questions from STEP 1 \
+STEP 3 - call the evaluator agent with the user prompt, the ground truth and the generated answer from STEP 2 \
+
+For each step, use the respective tool. \
+EACH TIME before invoking a function, you must explain your reasons for doing so. \
 Do not make up information. \
 """
 
+QA_SEARCH_INSTRUCTIONS = """\
+You are an agent specializing in matching a user query to related questions in a QA database. \
+You receive a single user query as input. \
+Use the qa_search_tool to return question, answer pairs in json format.\
+
+ALWAYS return the question, answer pairs in the following format. \
+user_query: str | None \
+question: str | None \
+answer: str | None \
+context: str | None \
+""" 
+
+KB_SEARCH_INSTRUCTIONS = """ \
+You are a knowledge base search agent. You receive a single question as input in the form of QASearchSingleResponse. \
+Use the search_knowledgebase tool to perform a search of key words related to the "question" field (not the user query). \
+Based on the search results, generate a final answer to the input question. Do NOT return raw search results. \
+
+ALWAYS return the final answer in the following format. \
+answer: str | None \
+context: str | None \
+"""
+
+EVALUATOR_INSTRUCTIONS = """\
+Evaluate whether the "Proposed Answer" to the given "Question" matches the "Ground Truth". \
+
+Input structure should be in the following format. \
+question: str \
+ground_truth: str \
+proposed_response: str \
+
+ALWAYS return the evaluation in the following format. \
+explanation: str \
+is_answer_correct: bool \
+"""
+
+EVALUATOR_TEMPLATE = """\
+# Question
+
+{question}
+
+# Ground Truth
+
+{ground_truth}
+
+# Proposed Answer
+
+{proposed_response}
+
+"""
