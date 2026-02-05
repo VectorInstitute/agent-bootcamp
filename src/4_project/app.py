0235faea70c359d71f7591cdfdaf9c39d512f78e
default <sooyshim@gmail.com>
2026-02-05 03:36:42 +0000

    add agent

--- src/4_project/app.py
+++ src/4_project/app.py
@@ -1,13 +1,34 @@
 from dotenv import load_dotenv
 from logging import basicConfig, INFO
 from src.utils.client_manager import AsyncClientManager
-from agents import set_tracing_disabled
+from agents import set_tracing_disabled, Agent, OpenAIChatCompletionsModel, Runner
 import gradio as gr
+from gradio.components.chatbot import ChatMessage
 from src.utils.gradio import COMMON_GRADIO_CONFIG
+from src.utils.agent_session import get_or_create_session
 import asyncio
 
-async def _main():
-    pass
+async def _main(query: str, history: list[ChatMessage], session_state: dict[str, Any]):
+    turn_messages: list[ChatMessage] = []
+
+    session = get_or_create_session(history, session_state)
+
+    main_agent = Agent(
+        name="Pension System Agent",
+        instruction="You're amazing",
+        model= OpenAIChatCompletionsModel(
+            model=client_manager.configs.default_worker_model,
+            openai_client=client_manager.openai_client,
+        ),
+    )
+
+    result = Runner.run_streamed(
+        main_agent,
+        input=query,
+        session=session
+    )
+
+    
 
 
 if __name__ == "__main__":
